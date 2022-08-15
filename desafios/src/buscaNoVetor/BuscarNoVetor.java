96417bd6dfea13216c4e07eca3e279fc0e098bce
Jadsonvs <jadson.vsantos2013@gmail.com>
2022-08-14 21:31:35 -0300

    Desafios mentoria

---
+++ desafios/src/buscaNoVetor/BuscarNoVetor.java
@@ -0,0 +1,24 @@
+package buscaNoVetor;
+
+public class BuscarNoVetor {
+	
+	public static void main(String[] args) {
+		int[] vetorInteiro = {10, 12, 25, 40, 50};
+		int procurarPor = 10;
+		
+		System.out.println("Posição do valor " + procurarPor + " dentro do vetor é " + buscarDentroNoVetor(vetorInteiro, procurarPor));
+		
+	}
+	
+	static int buscarDentroNoVetor(int[] vetorInteiro, int procurarPor) {
+		int posicaoDovalor = -1;
+		
+		for(int i = 0; i < vetorInteiro.length; i++) {
+			if(procurarPor == vetorInteiro[i]) {
+				return posicaoDovalor = i;
+			}
+		}
+		return posicaoDovalor;
+	}
+
+}
